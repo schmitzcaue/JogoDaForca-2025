5b7fb2a658ad7afdea9c03bbd536f8fd248c03ad
caues <caues@SCHMITZNOT>
2025-03-26 22:49:23 -0300

    Versão 1: Estrutura básica e entrada do usuário

--- JogoDaForca.ConsoleApp1/Program.cs
+++ JogoDaForca.ConsoleApp1/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        //Versão 1: Estrutura básica e entrada do usuário 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Jogo da forca");
+                Console.WriteLine("-------------------------------------");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0] ; // apena um caracter do q o usuario digita
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+            }
         }
+           
     }
 }
